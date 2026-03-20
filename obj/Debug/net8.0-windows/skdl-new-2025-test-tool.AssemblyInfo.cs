85ca05f926b324dce0aa75edd379c60b1b8a5510
Bruce <guangtao.chen.tcl@gmail.com>
2026-03-20 18:33:12 +0800

    1

--- obj/Debug/net8.0-windows/skdl-new-2025-test-tool.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/skdl-new-2025-test-tool.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("skdl-new-2025-test-tool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36356b067785c0855749c96d741457e44684eb8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e00a980cbd02205ef8afcb2ca6047389aeb4eb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("skdl-new-2025-test-tool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("skdl-new-2025-test-tool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
